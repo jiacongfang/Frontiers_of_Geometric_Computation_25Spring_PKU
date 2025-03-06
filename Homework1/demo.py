ecc85c4ceb38b1007a2f32743f7d7f759c725739
jiacongfang <jiacongfang0411@gmail.com>
2025-03-07 01:06:54 +0800

    FINISH: hw1 without extra problem

--- Homework1/demo.py
+++ Homework1/demo.py
@@ -1,44 +1,49 @@
-"""Fuse 1000 RGB-D images from the 7-scenes dataset into a TSDF voxel volume with 2cm resolution.
-"""
+"""Fuse 1000 RGB-D images from the 7-scenes dataset into a TSDF voxel volume with 2cm resolution."""
 
 import time
 import cv2
 import numpy as np
-from tqdm import tqdm 
+from tqdm import tqdm
+from skimage import measure
+import trimesh
 
 import fusion
 
 
 if __name__ == "__main__":
-
     print("Estimating voxel volume bounds...")
     n_imgs = 1000
-    
-    cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
-    
+
+    cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=" ")
+
     # vol_bnds[:, 0]: minimum bounds of the voxel volume along x, y, z
     # vol_bnds[:, 1]: maximum bounds of the voxel volume along x, y, z
     vol_bnds = np.zeros((3, 2))
-    
+
     for i in tqdm(range(n_imgs)):
-        
-        # Read depth image 
-        depth_im = cv2.imread("data/frame-%06d.depth.png"%(i), -1).astype(float)
+        # Read depth image
+        depth_im = cv2.imread("data/frame-%06d.depth.png" % (i), -1).astype(float)
         # depth is saved in 16-bit PNG in millimeters
-        depth_im /= 1000.  
+        depth_im /= 1000.0
         # set invalid depth to 0 (specific to 7-scenes dataset)
-        depth_im[depth_im == 65.535] = 0  
-        
+        depth_im[depth_im == 65.535] = 0
+
         # Read camera pose, a 4x4 rigid transformation matrix
-        cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))  
-        
+        cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % (i))
+
         #######################    Task 1    #######################
         #  Convert depth image to world coordinates
         view_frust_pts = fusion.cam_to_world(
-            depth_im, cam_intr, cam_pose,
-            export_pc=(i == 0)  # export pointcloud only for the first frame
+            depth_im,
+            cam_intr,
+            cam_pose,
+            export_pc=(i == 0),  # export pointcloud only for the first frame
         )
         # TODO: Update voxel volume bounds `vol_bnds`
+        max_xyz = np.max(view_frust_pts, axis=0)
+        min_xyz = np.min(view_frust_pts, axis=0)
+        vol_bnds[:, 0] = np.minimum(vol_bnds[:, 0], min_xyz)
+        vol_bnds[:, 1] = np.maximum(vol_bnds[:, 1], max_xyz)
         ############################################################
 
     print("Volume bounds:", vol_bnds)
@@ -51,13 +56,13 @@ if __name__ == "__main__":
     t0_elapse = time.time()
     for i in tqdm(range(n_imgs)):
         # Read depth image and camera pose
-        depth_im = cv2.imread("data/frame-%06d.depth.png"%(i),-1).astype(float)
-        depth_im /= 1000.
+        depth_im = cv2.imread("data/frame-%06d.depth.png" % (i), -1).astype(float)
+        depth_im /= 1000.0
         depth_im[depth_im == 65.535] = 0
-        cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))
+        cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % (i))
 
         # Integrate observation into voxel volume
-        tsdf_vol.integrate(depth_im, cam_intr, cam_pose, obs_weight=1.)
+        tsdf_vol.integrate(depth_im, cam_intr, cam_pose, obs_weight=1.0, i=i)
 
     fps = n_imgs / (time.time() - t0_elapse)
     print("Average FPS: {:.2f}".format(fps))
@@ -65,4 +70,10 @@ if __name__ == "__main__":
     #######################    Task 4    #######################
     # TODO: Extract mesh from voxel volume, save and visualize it
     ############################################################
+    print("Extracting mesh...")
+    tsdf_vol_visual = tsdf_vol.tsdf_vol.reshape(tsdf_vol.vol_dim)
+    verts, faces, norms, values = measure.marching_cubes(tsdf_vol_visual, level=0)
+    verts = verts * tsdf_vol.voxel_size + tsdf_vol.vol_bnds[:, 0]
 
+    mesh = trimesh.Trimesh(verts, faces)
+    mesh.export("mesh.ply")
--- Homework1/fusion.py
+++ Homework1/fusion.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2018 Andy Zeng
 
 import numpy as np
-from skimage import measure
 import trimesh
 
+
 class TSDFVolume:
-    """Volumetric TSDF Fusion of RGB-D Images.
-    """
+    """Volumetric TSDF Fusion of RGB-D Images."""
+
     def __init__(self, vol_bnds, voxel_size):
         """Constructor.
 
@@ -23,25 +23,39 @@ class TSDFVolume:
         self.voxel_size = float(voxel_size)
         self.trunc_margin = 5 * self.voxel_size  # truncation on SDF
 
-        
         #######################    Task 2    #######################
         # TODO: build voxel grid coordinates and initiailze volumn attributes
-        # Initialize voxel volume
-        self.tsdf_vol = None
+        # Compute voxel volume dimensions
+        self.vol_dim = np.round((vol_bnds[:, 1] - vol_bnds[:, 0]) / voxel_size).astype(
+            int
+        )
+
+        # Initialize voxel volume with 1.0 (truncation band) (NOT 0 !!)
+        self.tsdf_vol = np.ones(
+            self.vol_dim[0] * self.vol_dim[1] * self.vol_dim[2], dtype=np.float32
+        )
         # for computing the cumulative moving average of weights per voxel
-        self.weight_vol = None
+        self.weight_vol = np.zeros(
+            self.vol_dim[0] * self.vol_dim[1] * self.vol_dim[2], dtype=np.float32
+        )
         # Set voxel grid coordinates
-        self.vox_coords = None
+        xv, yv, zv = np.meshgrid(
+            np.arange(self.vol_dim[0]),
+            np.arange(self.vol_dim[1]),
+            np.arange(self.vol_dim[2]),
+            indexing="ij",
+        )
+        self.vox_coords = np.stack([xv, yv, zv], axis=-1).reshape(-1, 3)
         ############################################################
 
-    def integrate(self, depth_im, cam_intr, cam_pose, obs_weight=1.):
+    def integrate(self, depth_im, cam_intr, cam_pose, obs_weight=1.0, i=1):
         """Integrate an RGB-D frame into the TSDF volume.
 
         Args:
             depth_im (ndarray): A depth image of shape (H, W).
             cam_intr (ndarray): The camera intrinsics matrix of shape (3, 3).
             cam_pose (ndarray): The camera pose (i.e. extrinsics) of shape (4, 4).
-            obs_weight (float): The weight to assign for the current observation. 
+            obs_weight (float): The weight to assign for the current observation.
         """
 
         #######################    Task 2    #######################
@@ -49,36 +63,97 @@ class TSDFVolume:
         # TODO: Eliminate pixels outside depth images
         # TODO: Sample depth values
         ############################################################
-        
+        # Step I: Convert voxel grid coordinates to pixel coordinates
+        # voxel grid coordinates => world coordinates
+        world_coords = self.vox_coords * self.voxel_size + self.vol_bnds[:, 0]  # (N, 3)
+        world_coords_hom = np.hstack(
+            [world_coords, np.ones((world_coords.shape[0], 1))]
+        )  # (N, 4)
+
+        # world coordinates => camera coordinates
+        cam_pose_inv = np.linalg.inv(cam_pose)
+        cam_coords = (np.dot(cam_pose_inv, world_coords_hom.T).T)[:, :3]  # (N, 3)
+
+        # camera coordinates => pixel coordinates
+        pixel_coords = np.dot(cam_intr, cam_coords.T).T  # (N, 3)
+        pixel_coords[:, 0] /= pixel_coords[:, 2]
+        pixel_coords[:, 1] /= pixel_coords[:, 2]
+        # round to nearest pixel
+        pixel_coords = np.round(pixel_coords[:, :2]).astype(int)
+
+        # Step II: Eliminate pixels outside depth images [H, W]
+        H, W = depth_im.shape
+        valid_pix = (
+            (pixel_coords[:, 0] >= 0)
+            & (pixel_coords[:, 0] < W)
+            & (pixel_coords[:, 1] >= 0)
+            & (pixel_coords[:, 1] < H)
+        )
+
+        # Step III: Sample depth values: (depth_im[H, W] - the depth of the voxel grid coordinates)
+        depth_sample = np.zeros(self.vox_coords.shape[0])
+        depth_sample[valid_pix] = (
+            depth_im[
+                pixel_coords[valid_pix, 1].astype(int),
+                pixel_coords[valid_pix, 0].astype(int),
+            ]
+            - cam_coords[valid_pix, 2]
+        )
+
         #######################    Task 3    #######################
         # TODO: Compute TSDF for current frame
         ############################################################
+        valid_depth = depth_sample > -self.trunc_margin
+        valid_mask = valid_pix & valid_depth
+
+        tsdf = np.minimum(1.0, depth_sample / self.trunc_margin)
 
         #######################    Task 4    #######################
         # TODO: Integrate TSDF into voxel volume
         ############################################################
+        self.tsdf_vol[valid_mask] = (
+            self.tsdf_vol[valid_mask] * self.weight_vol[valid_mask]
+            + tsdf[valid_mask] * obs_weight
+        ) / (self.weight_vol[valid_mask] + obs_weight)
+        self.weight_vol[valid_mask] += obs_weight
 
 
 def cam_to_world(depth_im, cam_intr, cam_pose, export_pc=False):
     """Get 3D point cloud from depth image and camera pose
-    
+
     Args:
         depth_im (ndarray): Depth image of shape (H, W).
         cam_intr (ndarray): The camera intrinsics matrix of shape (3, 3).
         cam_pose (ndarray): The camera pose (i.e. extrinsics) of shape (4, 4).
         export_pc (bool): Whether to export pointcloud to a PLY file.
-        
+
     Returns:
         world_pts (ndarray): The 3D point cloud of shape (N, 3).
     """
-    
+
     #######################    Task 1    #######################
     # TODO: Convert depth image to world coordinates
-    world_pts = ...
+    # Step I: Convert the depth iamge to camera coordinates
+    cam_intr_inv = np.linalg.inv(cam_intr)
+
+    H, W = depth_im.shape
+
+    # Create a grid of pixel coordinates
+    i, j = np.meshgrid(np.arange(H), np.arange(W), indexing="ij")
+    pixels = np.stack([j, i, np.ones_like(i)], axis=-1).reshape(-1, 3)
+
+    # Convert pixel coordinates to camera coordinates
+    cam_cor = np.dot(cam_intr_inv, pixels.T).T
+    cam_cor *= depth_im.flatten()[:, None]
+
+    # Step II: Convert the camera coordinates to world coordinates
+    world_pts = np.dot(
+        cam_pose, np.hstack([cam_cor, np.ones((cam_cor.shape[0], 1))]).T
+    )[:3, :].T
     ############################################################
-    
+
     if export_pc:
         pointcloud = trimesh.PointCloud(world_pts)
         pointcloud.export("pointcloud.ply")
-    
+
     return world_pts
